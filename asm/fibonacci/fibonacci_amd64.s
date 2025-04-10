dac1c9d152ba606e247809fa0e51b73cb3cbdfec
Danil <>
2025-04-11 00:06:19 +0300

    del

--- asm/fibonacci/fibonacci_amd64.s
+++
@@ -1,4 +0,0 @@
-#include "textflag.h"
-
-// func Fibonacci(n uint64) uint64
-TEXT ·Fibonacci(SB), NOSPLIT, $0
--- asm/sum_slice/slice_sum_amd64.s
+++
@@ -1,4 +0,0 @@
-#include "textflag.h"
-
-// func SumSlice(s []int32) int64
-TEXT ·SumSlice(SB), NOSPLIT, $0
